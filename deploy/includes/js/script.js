e74859fa6fc74d178be325f1659417f4ff2930cb
Mike Allanson <michael.allanson@gmail.com>
2012-07-01 18:41:53 +0100

    Basic working version Initial page layout

--- deploy/includes/js/script.js
+++ deploy/includes/js/script.js
@@ -1,88 +1,26 @@
 /* Author:
 
 */
-"use strict";
+$(document).ready(function(){
+    $('#go').on('click', function(event){
+        event.preventDefault();
 
-var DC = DC || {};
+        console.log('clicked!');
+        var value = $('#unit-amount').val();
+        var unit = $('#unit-from').val();
 
-DC.settings = {
-    conversionCount: 2 // how many different units to convert to
-};
+        var conversions = DC.convert(value, unit);
 
-DC.base  = 'centimetres';
-
-DC.units = [
-    {
-        'name': 'centimetres',
-        'rate': 1,
-        'fromOrTo': 'from',
-        'type': 'distance',
-        'base': true
-    },
-    {
-        'name': 'metres',
-        'rate': 0.001,
-        'fromOrTo': 'from',
-        'type': 'distance',
-        'base': false
-    },{
-        'name': 'moonTrips',
-        'description': 'the distance from the earth to the moon',
-        'rate': 0.0000012,
-        'fromOrTo': 'to',
-        'type': 'distance',
-        'base': false
-    },{
-        'name': 'redBloodCells',
-        'description': 'lined up red blood cells',
-        'rate': 1200089,
-        'fromOrTo': 'to',
-        'type': 'distance',
-        'base': false
-    }
-]
-
-// converts a value from one unit to a selection of other units
-DC.convert = function(value, options){
-    // get the base unit
-    var baseUnit = DC.findUnit(DC.base);
-
-    var fromUnit = DC.findUnit(options.from);
-
-    // get all 'to' units whose type matches the base unit
-    var newUnits = _.filter(DC.units, function(unit){
-        return (unit.type == baseUnit.type) && (unit.fromOrTo == 'to');
+        displayResults(conversions);
     });
+});
 
-    // pick a few random units to convert to
-    newUnits = _.shuffle(newUnits).slice(0, DC.settings.conversionCount);
+// show the results on the page
+var displayResults = function(results){
+    var template = $("#result-items").html();
+    template = _.template(template, {'results': results});
 
-    var conversions = DC.getRates(value, fromUnit, newUnits);
-
-    _.each(conversions, function(conversion){
-        conversion.value = value * conversion.rate;
-    });
-
-    return conversions;
-};
-
-// returns the conversion rate between given units
-DC.getRates = function(value, fromUnit, toUnits) {
-    var conversions = [];
-
-    _.each(toUnits, function(unit){
-        conversions.push({
-            'name': unit.name,
-            'rate': unit.rate * (1 / fromUnit.rate)
-        });
-    });
-
-    return conversions;
-};
-
-// find a unit by its name
-DC.findUnit = function(name){
-    return _.find(DC.units, function(unit){
-        return unit.name == name;
+    $("#results").fadeOut(250, function(){
+        $(this).html(template).fadeIn();
     });
 };
